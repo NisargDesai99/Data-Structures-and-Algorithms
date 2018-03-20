a657f4794a6ce4caf514f2e7ad494201bfe1b1b2
Nisarg Desai <nisarg.desai599@gmail.com>
2018-03-20 09:34:32 -0500

    Update gitignore

--- Projects/Project_4/Main.java
+++
@@ -1,8 +0,0 @@
-import static java.lang.System.out;
-
-public class Main {
-    public static void main(String[] args) {
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
-        out.println("Hello, World!");
-    }
-}
